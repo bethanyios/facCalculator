903ac5dd24bc46e6b04b1b7d1c1ed4785d9454b1
Bethany Scott <bethanyscott@bethanys.home>
2019-10-16 22:09:15 +0100

    Initialise actions of buttons: make numbers appear in output and decimal point appear only once

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 class Calculator {
-  constructor(previousCalcText, currentCalcText) {
-    this.previousCalcText = previousCalcText
-    this.currentCalcText = currentCalcText
+  constructor(previousCalcTextElement, currentCalcTextElement) {
+    this.previousCalcTextElement = previousCalcTextElement
+    this.currentCalcTextElement = currentCalcTextElement
     this.clear()
   }
-}
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousCalcText = document.querySelector('[data-previous-calc]')
-const currentCalcText = document.querySelector('[data-current-calc]')
-
-clear(){
+clear() {
   this.currentCalc = ''
   this.previousCalc = ''
   this.operation = undefined
@@ -25,8 +16,10 @@ delete() {
 }
 
 appendNumber(number) {
-
+  if (number === '.' && this.currentCalc.includes('.')) return
+  this.currentCalc = this.currentCalc.toString() + number.toString()
 }
+
 chooseOperation(operation) {
 
 }
@@ -34,4 +27,25 @@ chooseOperation(operation) {
 compute() {
 
 }
+
+updateDisplay() {
+  this.currentCalcTextElement.innerText = this.currentCalc
 }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousCalcTextElement = document.querySelector('[data-previous-calc]')
+const currentCalcTextElement = document.querySelector('[data-current-calc]')
+
+const calculator = new Calculator(previousCalcTextElement, currentCalcTextElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
