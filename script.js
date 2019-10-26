d58ec9c6be2e34668c37a641bb0f5e7b2d70a150
Bethany Scott <bethanyscott@BethanyS.lan>
2019-10-26 16:01:45 +0100

    Implement the operative and clear functions

--- script.js
+++ script.js
@@ -5,32 +5,61 @@ class Calculator {
     this.clear()
   }
 
-clear() {
-  this.currentCalc = ''
-  this.previousCalc = ''
+  clear() {
+    this.currentCalc = ''
+    this.previousCalc = ''
+    this.operation = undefined
+  }
+
+  delete() {
+  this.currentCalc = this.currentCalc.toString().slice(0, -1)
+  }
+
+  appendNumber(number) {
+    if (number === '.' && this.currentCalc.includes('.')) return
+    this.currentCalc = this.currentCalc.toString() + number.toString()
+  }
+
+  chooseOperation(operation) {
+    if (this.currentCalc === '') return
+    if (this.previousCalc !== '') {
+      this.compute()
+    }
+    this.operation = operation
+    this.previousCalc = this.currentCalc
+    this.currentCalc = ''
+  }
+
+  compute() {
+  let computation
+  const prev = parseFloat(this.previousOperand)
+  const current = parseFloat(this.currentOperand)
+  if (isNaN(prev) || isNaN(current)) return
+  switch (this.operation) {
+    case '+':
+      computation = prev + current
+      break
+    case '-':
+      computation = prev - current
+      break
+    case '*':
+      computation = prev * current
+      break
+    case '÷':
+      computation = prev / current
+      break
+    default:
+      return
+  }
+  this.currentOperand = computation
   this.operation = undefined
+  this.previousOperand = ''
 }
 
-delete() {
-
-}
-
-appendNumber(number) {
-  if (number === '.' && this.currentCalc.includes('.')) return
-  this.currentCalc = this.currentCalc.toString() + number.toString()
-}
-
-chooseOperation(operation) {
-
-}
-
-compute() {
-
-}
-
-updateDisplay() {
-  this.currentCalcTextElement.innerText = this.currentCalc
-}
+  updateDisplay() {
+    this.currentCalcTextElement.innerText = this.currentCalc
+    this.previousCalcTextElement.innerText = this.previousCalc
+  }
 }
 
 const numberButtons = document.querySelectorAll('[data-number]')
@@ -40,7 +69,6 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousCalcTextElement = document.querySelector('[data-previous-calc]')
 const currentCalcTextElement = document.querySelector('[data-current-calc]')
-
 const calculator = new Calculator(previousCalcTextElement, currentCalcTextElement)
 
 numberButtons.forEach(button => {
@@ -48,4 +76,26 @@ numberButtons.forEach(button => {
     calculator.appendNumber(button.innerText)
     calculator.updateDisplay()
   })
+})
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+equalsButton.addEventListener('click', button => {
+  calculator.compute()
+  calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+  calculator.clear()
+  calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+  calculator.delete()
+  calculator.updateDisplay()
 })
